19b104f2cdb48c9377f4b2f7513ccb53db6ee52a
Your Name <noorarafat420@gmail.com>
2026-03-08 23:08:53 +0600

    Add object1.js file

---
+++ object1.js
@@ -0,0 +1,33 @@
+const car={
+    color: 'red',
+    weight: 344,
+    shape: 'dynamic'
+};
+
+var txt='';
+
+for(let el in car)
+{
+    txt += el;
+}
+
+console.log(txt);
+console.log(Object.values(car));
+
+
+
+const animal={
+    breed : 'cow',
+    quantity : 12,
+    name : 'dairy',
+    benefit : 'milk'
+}
+
+var upa;
+for(el in animal)
+{
+    upa = upa + el + " ";
+}
+
+console.log(upa);
+console.log(Object.values(animal));
